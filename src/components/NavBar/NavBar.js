510e80e234308a72efad3503be35b79bbde70659
Reynaldo Estrella <reynaldo@Reynaldos-MBP.lan>
2023-09-22 02:00:24 -0400

    Added Footer, Banner, Navbarfiles

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return(
+        <nav>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order</a></li>
+        </nav>
+    );
+}
+
+export default NavBar;
